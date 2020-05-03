9daf970ff6ecaa0435a439475a3eb6336d40fe2c
Ilya Haykinson <haykinson@gmail.com>
2020-05-03 01:05:03 -0700

    read data from dial to local dump1090

--- parse.go
+++ parse.go
@@ -1,71 +1,23 @@
 package main
 
 import (
-	"archive/zip"
-	"bufio"
-	"encoding/json"
 	"fmt"
 	geo "github.com/kellydunn/golang-geo"
-	"github.com/shopspring/decimal"
+	"github.com/ornen/go-sbs1"
 	"github.com/weilunwu/go-geofence"
-	"io/ioutil"
+	"io"
 	"log"
-	"os"
+	"net"
+	"sync"
 	"time"
 )
 
-
-/**
-{"Id":3756239,"Rcvr":11156,"HasSig":true,"Sig":22,"Icao":"3950CF","Bad":false,"Reg":"F-GUGP",
-"FSeen":"\/Date(1559378428473)\/","TSecs":3,"CMsgs":15,"Alt":18400,"GAlt":18657,
-"InHg":30.177166,"AltT":0,"Call":"AFR112H","Lat":45.557696,"Long":11.823212,"PosTime":1559378429830,
-"Mlat":false,"Tisb":false,"Spd":441.8,"Trak":276.5,"TrkH":false,"Type":"A318","Mdl":"Airbus A318 111",
-"Man":"Airbus","CNum":"2967","From":"EDDM Munich, Germany","To":"LFPG Charles de Gaulle, Paris, France",
-"Op":"Air France","OpIcao":"AFR","Sqk":"0240","Help":false,"Vsi":2304,"VsiT":0,"WTC":2,"Species":1,
-"Engines":"2","EngType":3,"EngMount":0,"Mil":false,"Cou":"France","HasPic":false,"Interested":false,
-"FlightsCount":0,"Gnd":false,"SpdTyp":0,"CallSus":false,"ResetTrail":true,"TT":"a","Trt":2,"Year":"2006",
-"Cos":[45.557602,11.824639,1559378429454.0,18500.0,45.557696,11.823212,1559378429830.0,18500.0]}
- */
-
-type Entries struct {
-	AcList []Entry `json:acList`
-}
-
-type Entry struct {
-	Id int
-	Reg string
-	Alt int
-	GAlt int
-	Trak decimal.Decimal
-	Lat decimal.Decimal
-	Long decimal.Decimal
-	PosTime int64
-}
-
-
-func parseZip(zipFile *zip.File) (*Entries, error) {
-	jsonFile, err := zipFile.Open()
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	defer jsonFile.Close()
-
-	bytes,_ := ioutil.ReadAll(jsonFile)
-
-	var entries Entries
-	json.Unmarshal(bytes, &entries)
-
-	return &entries, nil
-}
-
-
 func defineNorth() *geofence.Geofence {
 	polygon := []*geo.Point{
-		geo.NewPoint(33.9519407,	-118.4362650),
-		geo.NewPoint(33.9590602,	-118.3768702),
-		geo.NewPoint(33.9752902,	-118.3785868),
-		geo.NewPoint(33.9630468,	-118.4462214),
+		geo.NewPoint(33.9519407, -118.4362650),
+		geo.NewPoint(33.9590602, -118.3768702),
+		geo.NewPoint(33.9752902, -118.3785868),
+		geo.NewPoint(33.9630468, -118.4462214),
 	}
 
 	fence := geofence.NewGeofence([][]*geo.Point{polygon, []*geo.Point{}})
@@ -74,10 +26,10 @@ func defineNorth() *geofence.Geofence {
 
 func defineSouth() *geofence.Geofence {
 	polygon := []*geo.Point{
-		geo.NewPoint(33.9285849,	-118.4249353),
-		geo.NewPoint(33.9345669,	-118.3772135),
-		geo.NewPoint(33.9234571,	-118.3744669),
-		geo.NewPoint(33.9154800,	-118.4184122),
+		geo.NewPoint(33.9285849, -118.4249353),
+		geo.NewPoint(33.9345669, -118.3772135),
+		geo.NewPoint(33.9234571, -118.3744669),
+		geo.NewPoint(33.9154800, -118.4184122),
 	}
 
 	fence := geofence.NewGeofence([][]*geo.Point{polygon, []*geo.Point{}})
@@ -85,191 +37,382 @@ func defineSouth() *geofence.Geofence {
 }
 
 const (
-	Unknown = iota
-	North = iota
-	South = iota
+	locationUnknown = iota
+	locationNorth   = iota
+	locationSouth   = iota
 )
 
-type Flight struct {
-	Reg string
-	Direction int
-	StartTrack time.Time
-	Expires time.Time
-	Captures int
-	InNorth bool
-	InSouth bool
+type measure struct {
+	messagesReceived             int
+	messagesWithErrors           int
+	messagesWithAltitude         int
+	messagesWithTrack            int
+	messagesWithCallsign         int
+	messagesWithLocation         int
+	messagesWithMatchingAltitude int
+	messagesWithMatchingLocation int
+	flightsCreated               int
+	flightsExpired               int
+	flightsOutput                int
 }
 
-type Capture struct {
-	Reg string
-	Captured time.Time
+type flightInfo struct {
+	Callsign   string
+	Direction  int
+	AltitudeOk bool
+	Expires    time.Time
+	InNorth    bool
+	InSouth    bool
+
+	FullCaptures int
+	Processed    bool
+}
+
+type capturedRegistration struct {
+	HexId    string
+	Callsign string
+}
+
+type capturedTrack struct {
+	HexId string
+	Track float64
+}
+
+type capturedAltitude struct {
+	HexId    string
+	Altitude int32
+}
+
+type capturedLocation struct {
+	HexId    string
 	Location int
-	Direction int
 }
 
-func outputFlight(output chan Flight) {
+func (c capturedLocation) Key() string {
+	return c.HexId
+}
+
+func (c capturedAltitude) Key() string {
+	return c.HexId
+}
+
+func (c capturedTrack) Key() string {
+	return c.HexId
+}
+
+func (c capturedRegistration) Key() string {
+	return c.HexId
+}
+
+type keyedCapture interface {
+	Key() string
+}
+
+func outputFlight(output chan *flightInfo) {
 	for flight := range output {
 		var directionType string
 		switch flight.Direction {
-		case South:
+		case locationSouth:
 			directionType = "South"
-		case North:
+		case locationNorth:
 			directionType = "North"
 		default:
 			directionType = "Unknown"
 		}
-		fmt.Printf("Flight: Reg %s, Moving: %s\n", flight.Reg, directionType)
+		fmt.Printf("flightInfo: Reg %s, Moving: %s\n", flight.Callsign, directionType)
 	}
 }
 
-func processFlights(flights map[string]Flight, defaultExpiry time.Duration, captures chan Capture) {
-	output := make(chan Flight)
+func processFlights(flights map[string]*flightInfo, mutex *sync.Mutex, expiry time.Duration, captures chan keyedCapture, measures chan measure) {
+	output := make(chan *flightInfo)
 	go outputFlight(output)
 
 	for capture := range captures {
-		flight, ok := flights[capture.Reg]
-		if ok {
-			// if we've expired, consider this a new session
-			if capture.Captured.Before(flight.Expires) {
-				flight.Captures++
 
-				if capture.Location == South && !flight.InSouth {
-					flight.InSouth = true
-				} else if capture.Location == North && !flight.InNorth {
-					flight.InNorth = true
+		mutex.Lock()
+
+		flight, ok := flights[capture.Key()]
+
+		// if flightInfo was already processed, skip
+		if ok && flight.Processed {
+			continue
+		}
+
+		// consider this a new session if the flightInfo is very old
+		if ok && flight.Expires.Before(time.Now()) {
+			ok = false
+		}
+
+		// create a new record
+		if !ok {
+			flight = &flightInfo{
+				Direction: locationUnknown,
+				Expires:   time.Now().Add(expiry),
+			}
+			flights[capture.Key()] = flight
+			measures <- measure{flightsCreated: 1}
+		}
+
+		switch e := capture.(type) {
+		case capturedRegistration:
+			flight.Callsign = e.Callsign
+		case capturedTrack:
+			flight.Direction = getDirection(e.Track)
+		case capturedAltitude:
+			flight.AltitudeOk = true
+		case capturedLocation:
+			if e.Location == locationNorth {
+				flight.InNorth = true
+			} else if e.Location == locationSouth {
+				flight.InSouth = true
+			}
+		default:
+			panic("unknown capture type")
+		}
+
+		if fullySatisfied(flight) {
+			flight.FullCaptures++
+
+			if flight.FullCaptures >= 2 {
+				flight.Processed = true
+				measures <- measure{flightsOutput: 1}
+				output <- flight
+			}
+		}
+
+		mutex.Unlock()
+	}
+}
+
+func fullySatisfied(flight *flightInfo) bool {
+	return len(flight.Callsign) > 0 &&
+		flight.AltitudeOk &&
+		flight.InNorth &&
+		flight.InSouth &&
+		flight.Direction != locationUnknown
+}
+
+func getDirection(track float64) int {
+	if track >= 110 && track <= 160 {
+		return locationSouth
+	}
+	if track >= 290 && track <= 350 {
+		return locationNorth
+	}
+
+	return locationUnknown
+}
+
+func evaluateSBS1Message(message *sbs1.Message, north *geofence.Geofence, south *geofence.Geofence, captures chan keyedCapture, measures chan measure) bool {
+	if message.MessageType != sbs1.MessageTypeTransmission {
+		return false
+	}
+
+	recorded := false
+
+	if transmissionTypesWithCallsign[message.TransmissionType] {
+		captures <- capturedRegistration{
+			HexId:    message.HexId,
+			Callsign: message.Callsign,
+		}
+		measures <- measure{messagesWithCallsign: 1}
+		recorded = true
+	}
+
+	if transmissionTypesWithAltitude[message.TransmissionType] {
+		measures <- measure{messagesWithAltitude: 1}
+
+		if message.Altitude >= 3250 && message.Altitude <= 4750 {
+			captures <- capturedAltitude{
+				HexId:    message.HexId,
+				Altitude: message.Altitude,
+			}
+			measures <- measure{messagesWithMatchingAltitude: 1}
+			recorded = true
+		}
+	}
+
+	if transmissionTypesWithTrack[message.TransmissionType] {
+		captures <- capturedTrack{
+			HexId: message.HexId,
+			Track: message.Track.Degrees(),
+		}
+		measures <- measure{messagesWithTrack: 1}
+		recorded = true
+	}
+
+	if transmissionTypesWithCoords[message.TransmissionType] &&
+		!(message.Coordinates.Lat.E5() == 0 && message.Coordinates.Lng.E5() == 0) {
+
+		lat := message.Coordinates.Lat.Degrees()
+		long := message.Coordinates.Lng.Degrees()
+		point := geo.NewPoint(lat, long)
+		location := locationUnknown
+
+		if north.Inside(point) {
+			location = locationNorth
+			measures <- measure{messagesWithMatchingLocation: 1}
+		} else if south.Inside(point) {
+			location = locationSouth
+			measures <- measure{messagesWithMatchingLocation: 1}
+		}
+
+		captures <- capturedLocation{
+			HexId:    message.HexId,
+			Location: location,
+		}
+		measures <- measure{messagesWithLocation: 1}
+		recorded = true
+	}
+
+	return recorded
+}
+
+func processSBS1(north *geofence.Geofence, south *geofence.Geofence) {
+	conn, err := net.Dial("tcp", "192.168.7.153:30003")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	var reader = sbs1.NewReader(conn)
+
+	captures := make(chan keyedCapture)
+	flights := make(map[string]*flightInfo)
+	measures := make(chan measure)
+	expiration, _ := time.ParseDuration("20m")
+	cleanup, _ := time.ParseDuration("60m")
+
+	cleanupTicker := time.NewTicker(1 * time.Minute)
+	cleanupDone := make(chan bool)
+	reportTicker := time.NewTicker(30 * time.Second)
+	reportDone := make(chan bool)
+	flightsMutex := &sync.Mutex{}
+
+	go func() {
+		for {
+			select {
+			case <-cleanupDone:
+				return
+			case <-cleanupTicker.C:
+				flightsMutex.Lock()
+				log.Println("Cleaning up flights...")
+				removed := 0
+				for key, flight := range flights {
+					if time.Now().Sub(flight.Expires) > cleanup {
+						delete(flights, key)
+						removed++
+					}
 				}
+				log.Printf("Removed %v flights\n", removed)
+				flightsMutex.Unlock()
 
-				if flight.InSouth && flight.InSouth && flight.Captures >= 2 {
-					//fmt.Println("All conditions met", flight)
-					delete(flights, capture.Reg)
-					output <- flight
-				} else {
-					//fmt.Println("Conditions unmet:", flight)
+				measures <- measure{flightsExpired: removed}
+			}
+		}
+	}()
+
+	go func() {
+		aggregateMeasure := measure{}
+		for {
+			select {
+			case measure := <-measures:
+				aggregateMeasure.messagesReceived += measure.messagesReceived
+				aggregateMeasure.messagesWithErrors += measure.messagesWithErrors
+				aggregateMeasure.flightsCreated += measure.flightsCreated
+				aggregateMeasure.flightsExpired += measure.flightsExpired
+				aggregateMeasure.flightsOutput += measure.flightsOutput
+				aggregateMeasure.messagesWithAltitude += measure.messagesWithAltitude
+				aggregateMeasure.messagesWithCallsign += measure.messagesWithCallsign
+				aggregateMeasure.messagesWithLocation += measure.messagesWithLocation
+				aggregateMeasure.messagesWithTrack += measure.messagesWithTrack
+				aggregateMeasure.messagesWithMatchingAltitude += measure.messagesWithMatchingAltitude
+				aggregateMeasure.messagesWithMatchingLocation += measure.messagesWithMatchingLocation
+			case <-reportDone:
+				return
+			case <-reportTicker.C:
+				flightsMutex.Lock()
+				flightCount := 0
+				satisfiedFlightCount := 0
+				for _, flight := range flights {
+					flightCount++
+					if fullySatisfied(flight) {
+						satisfiedFlightCount++
+					} else {
+						log.Println("Partial", flight)
+					}
 				}
+				log.Printf("Flights: %v, satisfied: %v\n", flightCount, satisfiedFlightCount)
+				flightsMutex.Unlock()
 
+				log.Printf("%+v\n", aggregateMeasure)
+			}
+		}
+	}()
+
+	defer cleanupTicker.Stop()
+	defer reportTicker.Stop()
+
+	defer func() {
+		cleanupDone <- true
+		reportDone <- true
+	}()
+
+	go processFlights(flights, flightsMutex, expiration, captures, measures)
+
+	for {
+		var message, err = reader.Read()
+		measures <- measure{messagesReceived: 1}
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Println(err)
+				measures <- measure{messagesWithErrors: 1}
 				continue
 			}
 		}
 
-		flight = Flight{
-			Reg:        capture.Reg,
-			Direction:  capture.Direction,
-			StartTrack: capture.Captured,
-			Expires:    capture.Captured.Add(defaultExpiry),
-			InNorth:    capture.Location == North,
-			InSouth:    capture.Location == South,
-			Captures:   1,
-		}
-
-		flights[capture.Reg] = flight
+		//log.Println(message)
+		evaluateSBS1Message(message, north, south, captures, measures)
+		//log.Printf("%v\n", recorded)
 	}
 }
 
-func getDirection(track decimal.Decimal) int {
-	intTrack := track.IntPart()
-	if intTrack >= 110 && intTrack <= 160 {
-		return South
-	}
-	if intTrack >= 290 && intTrack <= 350 {
-		return North
-	}
+var (
+	transmissionTypesWithCallsign map[sbs1.TransmissionType]bool
+	transmissionTypesWithAltitude map[sbs1.TransmissionType]bool
+	transmissionTypesWithTrack    map[sbs1.TransmissionType]bool
+	transmissionTypesWithCoords   map[sbs1.TransmissionType]bool
+)
 
-	return Unknown
-}
+func initTransmissionTypes() {
+	transmissionTypesWithCallsign = make(map[sbs1.TransmissionType]bool)
+	transmissionTypesWithCallsign[sbs1.TransmissionTypeESIdentAndCategory] = true
 
-func evaluate(filename string, entries *Entries, north *geofence.Geofence, south *geofence.Geofence, captures chan Capture) {
-	for _, entry := range entries.AcList {
-		if len(entry.Reg) == 0 || entry.Lat.IsZero() || entry.Long.IsZero() {
-			continue
-		}
+	transmissionTypesWithAltitude = make(map[sbs1.TransmissionType]bool)
+	transmissionTypesWithAltitude[sbs1.TransmissionTypeESSurfacePos] = true
+	transmissionTypesWithAltitude[sbs1.TransmissionTypeESAirbornePos] = true
+	transmissionTypesWithAltitude[sbs1.TransmissionTypeSurveillanceAlt] = true
+	transmissionTypesWithAltitude[sbs1.TransmissionTypeSurveillanceId] = true
+	transmissionTypesWithAltitude[sbs1.TransmissionTypeAirToAir] = true
 
-		if (entry.Alt < 3250 && entry.GAlt < 3250) || (entry.Alt > 4750 && entry.GAlt > 4750) {
-			continue
-		}
+	transmissionTypesWithTrack = make(map[sbs1.TransmissionType]bool)
+	transmissionTypesWithTrack[sbs1.TransmissionTypeESSurfacePos] = true
+	transmissionTypesWithTrack[sbs1.TransmissionTypeESAirborneVel] = true
 
-		lat, _ := entry.Lat.Float64()
-		long, _ := entry.Long.Float64()
-		point := geo.NewPoint(lat, long)
-
-		if north.Inside(point) {
-			direction := getDirection(entry.Trak)
-			if direction != Unknown {
-				captures <- Capture {
-					Reg: entry.Reg,
-					Captured: time.Unix(0, entry.PosTime*1000),
-					Location: North,
-					Direction: direction,
-				}
-				//fmt.Print("Captured ")
-			}
-			//fmt.Println(filename, "North", entry.Reg, entry.Lat, entry.Long, entry.Alt, entry.GAlt, entry.Trak)
-		}
-		if south.Inside(point) {
-			direction := getDirection(entry.Trak)
-			if direction != Unknown {
-				captures <- Capture {
-					Reg: entry.Reg,
-					Captured: time.Unix(0, entry.PosTime*1000),
-					Location: South,
-					Direction: direction,
-				}
-				//fmt.Print("Captured ")
-			}
-
-			//fmt.Println(filename, "South", entry.Reg, entry.Lat, entry.Long, entry.Alt, entry.GAlt, entry.Trak)
-		}
-
-	}
-}
-
-
-func processZipFile(zipFile *zip.File, north *geofence.Geofence, south *geofence.Geofence, sem chan bool, captures chan Capture) {
-	entries, err := parseZip(zipFile)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	evaluate(zipFile.Name, entries, north, south, captures)
-
-	<- sem
-}
-
-func processZip(zipFilename string, north *geofence.Geofence, south *geofence.Geofence) {
-	// Open a zip archive for reading.
-	r, err := zip.OpenReader(zipFilename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer r.Close()
-
-	concurrency := 8
-	sem := make(chan bool, concurrency)
-	captures := make(chan Capture)
-	flights := make(map[string]Flight)
-	defaultDuration, _ := time.ParseDuration("20m")
-
-	go processFlights(flights, defaultDuration, captures)
-
-	for _, f := range r.File {
-		fmt.Println(f.Name)
-
-		sem <- true
-		go processZipFile(f, north, south, sem, captures)
-	}
-
-	for i := 0; i < cap(sem); i++ {
-		sem <- true
-	}
-
+	transmissionTypesWithCoords = make(map[sbs1.TransmissionType]bool)
+	transmissionTypesWithCoords[sbs1.TransmissionTypeESSurfacePos] = true
+	transmissionTypesWithCoords[sbs1.TransmissionTypeESAirbornePos] = true
 }
 
 func main() {
 	north := defineNorth()
 	south := defineSouth()
+	initTransmissionTypes()
 
-	processZip("/Users/ilya/Downloads/adsb/2019-06-01.zip", north, south)
-
-	input := bufio.NewScanner(os.Stdin)
-	input.Scan()
-
+	processSBS1(north, south)
 }
